8ff7d7cd8b5f28ad4b8e7c9c84879eff01b0ec7b
Nurlykhan <nurlykhank@gmail.com>
2018-10-18 00:25:08 +0600

    Added simple version of users and posts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+
 	def hello
 		render html: "Hello, World!"
 	end
---
+++ app/models/post.rb
@@ -0,0 +1,5 @@
+class Post < ApplicationRecord
+	belongs_to :user
+	validates :content, length: {maximum: 140},
+						presence: true
+end
